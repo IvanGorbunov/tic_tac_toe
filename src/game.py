0b3c8970fcdcbc2577f72e8564b75c30e17b8133
IvanGorbunov <ivan_0110@mail.ru>
2022-08-27 23:39:08 +0300

    added a close button

--- src/game.py
+++ src/game.py
@@ -13,23 +13,28 @@ class Player:
     async def get_ws(self):
         return self.__ws
 
+    async def check_ws(self, ws: WebSocket) -> bool:
+        return ws == self.__ws
+
 
 class Game:
 
+    id = 0
     player_1 = None
     player_2 = None
     current_player = ''
     active_game = False
 
     @classmethod
-    async def create(cls, ws: WebSocket):
+    async def create(cls, ws: WebSocket, game_id: int):
         self = cls()
+        self.id = game_id
         player = await self.create_player(ws)
         self.player_1 = player
         self.current_player = await player.get_state()
         return self
 
-    async def create_player(self, ws: WebSocket):
+    async def create_player(self, ws: WebSocket) -> Player:
         return Player(ws, 'X')
 
     async def join_player(self, ws: WebSocket):
@@ -38,5 +43,6 @@ class Game:
             self.player_2 = player
             self.active_game = True
 
-
+    async def check_player_ws(self, ws: WebSocket) -> bool:
+        return await self.player_1.check_ws(ws) or await self.player_2.check_ws(ws)
 
--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
 const ws = new WebSocket('ws://localhost:8000/ws')
-let iPlayer
+let iPlayer = "X"
 let otherPlayer
+let activeGame = false
+let gameState = ["", "", "", "", "", "", "", "", ""]
 
 
 ws.onopen = function (event){
@@ -16,7 +18,13 @@ ws.onmessage = function (event){
             gameList(data.games)
             break
         case 'join' :
-            startGame(data.action, data.player, data.other_player)
+            startGame(data.action, data.player, data.other_player, data.game_id)
+            break
+        case 'create' :
+            createdGame(data.player, data.game_id)
+            break
+        case 'move' :
+            moveGame(data.cell, data.move)
             break
         default:
             break
@@ -35,29 +43,57 @@ function createGame(event){
     send({action: 'create'})
 }
 
+function moveGame(cellIndex, move) {
+
+}
+
+function createdGame(state, game_id) {
+    iPlayer = state
+    newGame(game_id);
+}
+
 function joinGame(event){
     let btn = event.target
     send({action: 'join', game: btn.id})
 }
 
-function startGame(action, player, other_player){
+function newGame(game_id) {
+    document.getElementById('game').className = 'game-off'
+    document.getElementById('tic-tac-toe').className = 'game-on'
+    let state = document.getElementById('player')
+    state.className = 'game-on'
+    state.innerHTML = `<p>ID game: <span class="text-underline">${game_id}</span></p><p>You are player: <span class="text-underline">${iPlayer}</span></p>`
+}
+
+function showListGames(){
+    document.getElementById('game').className = 'game-on'
+    document.getElementById('tic-tac-toe').className = 'game-off'
+    let state = document.getElementById('player')
+    state.className = 'game-off'
+    state.innerHTML = ''
+}
+
+function startGame(action, player, other_player, game_id){
     iPlayer = player
     otherPlayer = other_player
 
-    document.getElementById('game').className = 'game-off'
-    let state = document.getElementById('player')
-    state.className = 'game-on'
-    state.innerHTML = `You are player ${iPlayer}`
+    newGame(game_id);
 }
 
 function gameList(game) {
-    let i = 1
-    while (i <= game){
+    showListGames()
+    let i = 0
+    if (game === 0) {
+        // let gameList = document.getElementById('gameList')
+        // gameList.removeChild(gameList.lastChild)
+        return
+    }
+    while (i < game){
         let gameList = document.getElementById('gameList')
         let li = document.createElement('li')
         let text = document.createTextNode(`Game ${i}    `)
         let btn = document.createElement('button')
-        btn.id = `${i}`
+        btn.id = `${i + 1}`
         btn.innerHTML = 'Connect'
         btn.addEventListener('click', joinGame)
         li.appendChild(text)
@@ -68,8 +104,26 @@ function gameList(game) {
 }
 
 
+function clickCell(event) {
+    const cell = event.target
+    const cellIndex = parseInt(cell.getAttribute('data-cell-index'))
 
+    if (!activeGame || gameState[cellIndex] !== "") {
+        return
+    }
+    gameState[cellIndex] = iPlayer
+    cell.innerHTML = iPlayer
 
+    send({action: 'move', 'cell': cellIndex})
+}
+
+function closeGame() {
+    send({action: 'close'})
+}
 
 document.getElementById('create-game').addEventListener('click', createGame)
+document.getElementById('close-game').addEventListener('click', closeGame)
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', clickCell))
 
